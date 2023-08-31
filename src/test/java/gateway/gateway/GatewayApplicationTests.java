ae08ca897b458365d125744c56faead343b5cf1d
DavidKing <david.king@kinandcarta.com>
2023-08-31 14:17:05 -0500

    version 2

---
+++ src/test/java/gateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package gateway.gateway;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GatewayApplicationTests {
+    @Test
+    void shouldCreateAnApplication() {
+        assertThat(Boolean.TRUE).isTrue();
+    }
+}
